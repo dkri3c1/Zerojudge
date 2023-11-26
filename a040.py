93501c62b63101207e275ce76016f0861c4a8b25
dkriecl <abel960717@gmail.com>
2023-11-26 19:04:24 +0800

    Update a040.py

--- a040.py
+++ a040.py
@@ -1,16 +1,3 @@
-'''
-n,m=map(int,input().split())
-for i in range(n,m):
-'''
-'''
-n=input()
-l=len(n)
-sum=0
-for i in n:
-    sum += int(i)**l
-    if sum == int(n):
-        print(int(n))
-'''
 ok=0
 n,m=map(int,input().split())
 for j in range(n,m):
